b9eb71d82164f753654fbf5401b6dd7f5373d426
Tim Heinrich <tim.heinrich@jtl-software.com>
2021-09-21 08:36:40 +0200

    Initial commit

---
+++ WinFormsDesignerTest/MyFormBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsDesignerTest
+{
+    public partial class MyFormBase : Form
+    {
+        public MyFormBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
